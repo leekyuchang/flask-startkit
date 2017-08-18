b84012e42395e4df08a24fe5556bd6252be53d7e
kyuchanglee <kyuchang90@gmail.com>
2017-08-18 16:33:44 +0900

    modify gitignore .pyc & request route test

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 # from flask import render_template
 from app import app
+from flask import request, jsonify
 
 @app.route('/')
 def index():
@@ -8,3 +9,8 @@ def index():
 @app.route('/test')
 def test():
     return 'This is test page.'
+
+@app.route('/hello')
+def hello():
+    r = request.get('http://www.google.com')
+    return r.text
